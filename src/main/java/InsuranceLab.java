0d10f2cd270b461e962385224d6414bd08a8f6fb
JSpickelmier <78034029+JSpickelmier@users.noreply.github.com>
2021-03-01 14:57:51 -0600

    4th commit

--- src/main/java/InsuranceLab.java
+++ src/main/java/InsuranceLab.java
@@ -17,17 +17,16 @@ Scanner CustAge = new Scanner(System.in);
 int Age = CustAge.nextInt();
 TotCost(Age);
 int price = TotCost(Age);
-
-
-
-
-System.out.println("$" + price + " is the total cost of insurance for your age");
-
-
+PrintCost(price);
   }
+    
     public static int TotCost(int agenumb){
     int Cost = 5 * agenumb + 300;
     return Cost;
+    }
+    
+    public static void PrintCost(int Cost){
+    System.out.println("$" + Cost + " is the total cost of insurance for your age");
     }
     
  }  
